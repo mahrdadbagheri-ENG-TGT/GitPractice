43018d41683158cf1ee464c2b90e345d8c72de99
mahrdadbagheri-ENG-TGT <mahrdadbagheri77@gmail.com>
2025-10-18 22:48:27 -0700

    Update hello_git.m
    git pull command

--- hello_git.m
+++ hello_git.m
@@ -4,3 +4,4 @@ clear;
 disp("Hello, Git from MATLAB!");
 disp ("Version 2 - now synced with GitHub!" );
 disp ("After any new commits in GitHub, you can download those commits by fetch command and merge them with git origin/main");
+disp ("Using pull command");
